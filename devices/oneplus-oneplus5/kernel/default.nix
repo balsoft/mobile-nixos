c6e21176775089303c82b26aae76de950c0f1deb
Alexander Bantyev <balsoft@yandex.ru>
2019-10-27 18:48:09 +0300

    Add oneplus-oneplus5

---
+++ devices/oneplus-oneplus5/kernel/default.nix
@@ -0,0 +1,50 @@
+{
+  mobile-nixos
+, fetchFromGitHub
+, kernelPatches ? [] # FIXME
+, buildPackages
+, dtbTool
+}:
+
+(mobile-nixos.kernel-builder-gcc6 {
+  configfile = ./config.aarch64;
+
+  file = "Image.gz-dtb";
+  hasDTB = true;
+
+  version = "4.4.173-perf";
+  src = fetchFromGitHub {
+    owner = "android-linux-stable";
+    repo = "op5";
+    rev = "d30e6a8dc2818b2540b974990faf77bb7f503d3b";
+    sha256 = "1lv18v7jwhspzry1i7pr7jgbycm18hxiymbnwq727sqqlwmz7lzw";
+  };
+
+  patches = [
+  ];
+
+  isModular = false;
+
+}).overrideAttrs({ postInstall ? "", postPatch ? "", ... }: {
+  installTargets = [ "zinstall" "Image.gz-dtb" "install" ];
+  postPatch = postPatch + ''
+    cp -v "${./compiler-gcc6.h}" "./include/linux/compiler-gcc6.h"
+
+    # FIXME : factor out
+    (
+    # Remove -Werror from all makefiles
+    local i
+    local makefiles="$(find . -type f -name Makefile)
+    $(find . -type f -name Kbuild)"
+    for i in $makefiles; do
+      sed -i 's/-Werror-/-W/g' "$i"
+      sed -i 's/-Werror=/-W/g' "$i"
+      sed -i 's/-Werror//g' "$i"
+    done
+    )
+  '';
+  postInstall = postInstall + ''
+    mkdir -p "$out/dtbs/"
+    cp -v "$buildRoot/arch/arm64/boot/Image.gz-dtb" "$out/"
+  '';
+})
